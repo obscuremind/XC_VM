35d26fc42ca060086286a72635ef25366557f9f2
obscuremind <dreamer1@live.com.pt>
2025-10-05 22:49:50 +0100

    Fix regex escaping in PTN parser

--- src/includes/python/PTN/patterns.py
+++ src/includes/python/PTN/patterns.py
@@ -2,34 +2,37 @@
 # -*- coding: utf-8 -*-
 
 patterns = [
-    ("season", "(s?([0-9]{1,2}))[ex ]"),
-    ("episode", "([ex]([0-9]{2})(?:[^0-9]|$))"),
-    ("year", "([\[\(]?((?:19[0-9]|20[0-2])[0-9])[\]\)]?)"),
-    ("resolution", "([0-9]{3,4}p)"),
+    ("season", r"(s?([0-9]{1,2}))[ex ]"),
+    ("episode", r"([ex]([0-9]{2})(?:[^0-9]|$))"),
+    ("year", r"([\[\(]?((?:19[0-9]|20[0-2])[0-9])[\]\)]?)"),
+    ("resolution", r"([0-9]{3,4}p)"),
     (
         "quality",
         (
-            "((?:PPV\.)?[HP]DTV|(?:HD)?CAM|B[DR]Rip|TS|(?:PPV "
-            ")?WEB-?DL(?: DVDRip)?|HDRip|DVDRip|DVDR"
-            "IP|CamRip|W[EB]BRip|BluRay|DvDScr|hdtv)"
+            r"((?:PPV\.)?[HP]DTV|(?:HD)?CAM|B[DR]Rip|TS|(?:PPV "
+            r")?WEB-?DL(?: DVDRip)?|HDRip|DVDRip|DVDR"
+            r"IP|CamRip|W[EB]BRip|BluRay|DvDScr|hdtv)"
         ),
     ),
-    ("codec", "(xvid|[hx]\.?26[45])"),
+    ("codec", r"(xvid|[hx]\.?26[45])"),
     (
         "audio",
-        ("(MP3|DD5\.?1|Dual[\- ]Audio|LiNE|H?DTS|AAC(?:\.?2\.0)" "?|AC3(?:\.5\.1)?)"),
+        (
+            r"(MP3|DD5\.?1|Dual[\- ]Audio|LiNE|H?DTS|AAC(?:\.?2\.0)"
+            r"?|AC3(?:\.5\.1)?)"
+        ),
     ),
-    ("group", "(- ?([^-]+(?:-={[^-]+-?$)?))$"),
-    ("region", "R[0-9]"),
-    ("extended", "(EXTENDED(:?.CUT)?)"),
-    ("hardcoded", "HC"),
-    ("proper", "PROPER"),
-    ("repack", "REPACK"),
-    ("container", "(MKV|AVI)"),
-    ("widescreen", "WS"),
-    ("website", "^(\[ ?([^\]]+?) ?\])"),
-    ("language", "rus\.eng"),
-    ("sbs", "(?:Half-)?SBS"),
+    ("group", r"(- ?([^-]+(?:-={[^-]+-?$)?))$"),
+    ("region", r"R[0-9]"),
+    ("extended", r"(EXTENDED(:?.CUT)?)"),
+    ("hardcoded", r"HC"),
+    ("proper", r"PROPER"),
+    ("repack", r"REPACK"),
+    ("container", r"(MKV|AVI)"),
+    ("widescreen", r"WS"),
+    ("website", r"^(\[ ?([^\]]+?) ?\])"),
+    ("language", r"rus\.eng"),
+    ("sbs", r"(?:Half-)?SBS"),
 ]
 
 types = {
